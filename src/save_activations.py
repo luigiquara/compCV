c81354b341624d56994e17a616c94f712610486e
Luigi Quarantiello <luigi.quarantiello@phd.unipi.it>
2023-12-11 15:01:01 +0000

    implement single module approach

--- src/save_activations.py
+++ src/save_activations.py
@@ -1,20 +1,22 @@
 import torch
 from torch import nn
 
-class SaveActivations(nn.Module):
+class SaveFeatures(nn.Module):
     def __init__(self, model, layers_id):
         super().__init__()
         self.model = model
         self.layers_id = layers_id
-        self.activations = {l_id: torch.empty(0) for l_id in layers_id}
+        self.features = {l_id: torch.empty(0) for l_id in layers_id}
 
-        for l_id in layers_id:
+        for l_id in self.layers_id:
+            layer = dict([*self.model.named_modules()])[l_id]
             layer.register_forward_hook(self.save_hook(l_id))
 
     def forward(self, x):
         out = self.model(x)
+        return self.features
 
     def save_hook(self, l_id):
         def hook(model, input, output):
-            self.activations[l_id] = output
+            self.features[l_id] = output
         return hook
