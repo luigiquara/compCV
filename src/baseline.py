4366c0f1b3451d38f60ea682cb1f7ade1a00ff84
Luigi Quarantiello <luigi.quarantiello@phd.unipi.it>
2024-01-29 10:24:11 +0000

    refactor code

--- src/baseline.py
+++ src/baseline.py
@@ -4,23 +4,27 @@ import wandb
 import torch
 
 from utils import get_model, load_cub
-from save_features import SaveFeatures
+from save_features import Extractor
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-def zero_shot_knn(model):
+def zero_shot(model, classifier):
     device = torch.device('cuda')
     resnet = get_model(model).to(device)
-    tr_loader, test_loader = load_cub(batch_size=64)
+    tr_loader, test_loader = load_cub(batch_size=32)
 
     resnet.eval()
     # extract features from the last layer before the classification head
-    extractor = SaveFeatures(resnet, layers_id=['avgpool'])
+    extractor = Extractor(resnet, layers_id=['avgpool'])
 
-    training_features = {'avgpool': torch.tensor([]).to(device)}
+    #training_features = {'avgpool': torch.tensor([]).to(device)}
+    training_features = {'avgpool': torch.tensor([])}
     training_target = []
-    test_features = {'avgpool': torch.tensor([]).to(device)}
+    #test_features = {'avgpool': torch.tensor([]).to(device)}
+    test_features = {'avgpool': torch.tensor([])}
     test_target = []
+
 
     # forward pass
     with torch.no_grad():
@@ -43,18 +47,30 @@ def zero_shot_knn(model):
         test_features = test_features['avgpool'].squeeze()
         test_target = torch.cat(test_target)
 
-    # knn fitting
-    knn = KNeighborsClassifier(n_neighbors=20, weights='distance')
-    knn.fit(training_features.numpy(force=True), training_target.numpy(force=True))
-    preds = knn.predict(test_features.numpy(force=True))
+    if classifier == 'knn':
+        print('Using KNeighborsClassifier')
+        classifier = KNeighborsClassifier(n_neighbors=20, weights='distance')
+        wandb.config.update({
+            'batch_size': 32,
+            'classifier': 'knn',
+            'n_neighbors': 20,
+            'weights_knn': 'distance'
+        })
+    elif classifier == 'logreg':
+        print('Using LogisticRegression')
+        classifier = LogisticRegression(max_iter=500)
+        wandb.config.update({
+            'batch_size': 32,
+            'classifier': 'LogisticRegression',
+            'max_iter': 500
+        })
+    
+    # fit the selected classifier and get the test accuracy
+    classifier.fit(training_features.numpy(force=True), training_target.numpy(force=True))
+    preds = classifier.predict(test_features.numpy(force=True))
     acc = accuracy_score(preds, test_target.numpy(force=True))
     print(acc)
 
-    wandb.config.update({
-        'batch_size': 64,
-        'n_neighbors': 20,
-        'weights_knn': 'distance'
-    })
     wandb.log({'accuracy': acc})
 
 if __name__ == '__main__':
@@ -63,8 +79,13 @@ if __name__ == '__main__':
         '--model',
         type=str
     )
+    parser.add_argument(
+        '--classifier',
+        type=str
+    )
     flags = parser.parse_args()
     model = flags.model
+    classifier = flags.classifier
 
-    wandb.init(project = 'CV & Compositionality', group='baseline_knn')
-    zero_shot_knn(model)
+    wandb.init(project = 'CV & Compositionality')
+    zero_shot(model, classifier)
