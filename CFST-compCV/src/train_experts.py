094641761989ae9e804876254ade169851f6f285
Luigi Quarantiello <luigi.quarantiello@phd.unipi.it>
2024-04-22 07:34:26 +0000

    add the one expert case

--- CFST-compCV/src/train_experts.py
+++ CFST-compCV/src/train_experts.py
@@ -56,7 +56,7 @@ def _get_subset_idx(dataset, current_classes_idxs):
 
     cc_idx = [i for i in range(len(dataset)) if dataset.targets[i] in current_classes_idxs]
     remaining_idx = list(set(range(len(dataset))) - set(cc_idx))
-    no_cc_idx = random.choices(remaining_idx, k=len(cc_idx))
+    no_cc_idx = random.choices(remaining_idx, k=int(len(cc_idx)/3))
     return cc_idx + no_cc_idx
 
 def run(param):
@@ -84,33 +84,51 @@ def run(param):
         current_classes_idxs = [e_tr_set.class_to_idx[c] for c in current_classes]
         print(f'Training Expert {i+1}/{param.n_experts} on {current_classes}')
 
+        # only if there is more than one expert
         # create a subset of the entire dataset
         # select all the samples for current classes, and a percentage of the other classes
-        tr_idxs = _get_subset_idx(e_tr_set, current_classes_idxs) 
-        val_idsx = _get_subset_idx(e_val_set, current_classes_idxs)
-        test_idxs = _get_subset_idx(e_test_set, current_classes_idxs)
-        tr_set = Subset(e_tr_set, tr_idxs)
-        val_set = Subset(e_val_set, val_idsx)
-        test_set = Subset(e_test_set, test_idxs)
+        if param.n_experts > 1:
+            tr_idxs = _get_subset_idx(e_tr_set, current_classes_idxs) 
+            val_idsx = _get_subset_idx(e_val_set, current_classes_idxs)
+            test_idxs = _get_subset_idx(e_test_set, current_classes_idxs)
+            tr_set = Subset(e_tr_set, tr_idxs)
+            val_set = Subset(e_val_set, val_idsx)
+            test_set = Subset(e_test_set, test_idxs)
+        else: # if only one expert, the current sets are the entire sets
+            tr_set = e_tr_set
+            val_set = e_val_set
+            test_set = e_test_set
 
         # create dataloaders
         tr_loader = DataLoader(tr_set, batch_size=param.batch_size, shuffle=True)
         val_loader = DataLoader(val_set, batch_size=param.batch_size, shuffle=False)
         test_loader = DataLoader(test_set, batch_size=param.batch_size, shuffle=False)
 
-        # create the model
-        # change the classifier, to perform (1 vs All)-classification
-        # we want to classify the current classes, plus the 'other' class
+        # create the model and change the classifier, with the rigth number of classes
         model = resnet18()
         fc_in_features = model.fc.in_features
-        model.fc = nn.Linear(fc_in_features, len(current_classes)+1)
+
+        # if more than one expert, we perform (1 vs all)-classification
+        # we want to classify the current classes, plus the 'other' class
+        if param.n_experts > 1: fc_out_features = len(current_classes)+1
+        # if only one expert, standard classification
+        else: fc_out_features = len(current_classes)
+
+        model.fc = nn.Linear(fc_in_features, fc_out_features)
+
+        # resnet18 from CGQA has 5x5 kernels
+        # change it to be compatible and to load the model later, for continual training
+        if param.n_experts == 1:
+            model.conv1 = nn.Conv2d(3, 64, kernel_size=5, stride=2, padding=1, bias=False)
 
         # training process
-        # inside, the labels are preprocessed for (1 vs all)-classification
-        save_path = param.save_root + f'expert_{i+1}/{run.name}'
+        # inside, the labels are preprocessed for (1 vs all)-classification (if needed)
+        save_path = param.save_root + f'greatest_expert/{run.name}'
         optimizer = Adam(model.parameters(), lr=param.lr)
         trainer = Trainer(model, nn.CrossEntropyLoss(), optimizer, current_classes=current_classes_idxs, save_path=save_path, device='cuda', log=param.log)
-        results = trainer.train(tr_loader, val_loader, num_classes=len(current_classes)+1, epochs=param.max_epochs)
+
+        num_classes = len(current_classes)+1 if param.n_experts > 1 else len(current_classes)
+        results = trainer.train(tr_loader, val_loader, num_classes=num_classes, epochs=param.max_epochs)
 
         run.finish()
 
